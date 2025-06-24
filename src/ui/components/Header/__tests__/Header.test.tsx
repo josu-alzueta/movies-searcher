805b9db610e846a31427366f2d18ac9b54eda0cc
Josu Alzueta <josu.alzueta@biko2.com>
2025-06-24 19:26:00 +0200

    Removes unnecesary tests

--- src/ui/components/Header/__tests__/Header.test.tsx
+++ src/ui/components/Header/__tests__/Header.test.tsx
@@ -2,47 +2,18 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import { Header } from "../Header";
 
+const defaultProps = {
+  title: "🎬 Movie Search",
+  subtitle: "Encuentra tu película favorita",
+};
+
 describe("Header Component", () => {
-  it("renders with default title and subtitle", () => {
-    render(
-      <Header
-        title="🎬 Movie Search"
-        subtitle="Encuentra tu película favorita"
-      />
-    );
+  it("renders with title and subtitle", () => {
+    render(<Header {...defaultProps} />);
 
     expect(screen.getByText("🎬 Movie Search")).toBeInTheDocument();
     expect(
       screen.getByText("Encuentra tu película favorita")
     ).toBeInTheDocument();
-  });
-
-  it("renders with custom title", () => {
-    const customTitle = "Custom Movie App";
-    render(
-      <Header title={customTitle} subtitle="Encuentra tu película favorita" />
-    );
-
-    expect(screen.getByText(customTitle)).toBeInTheDocument();
-    expect(
-      screen.getByText("Encuentra tu película favorita")
-    ).toBeInTheDocument();
-  });
-
-  it("renders with custom subtitle", () => {
-    const customSubtitle = "Busca tus películas aquí";
-    render(<Header title="🎬 Movie Search" subtitle={customSubtitle} />);
-
-    expect(screen.getByText("🎬 Movie Search")).toBeInTheDocument();
-    expect(screen.getByText(customSubtitle)).toBeInTheDocument();
-  });
-
-  it("renders with both custom title and subtitle", () => {
-    const customTitle = "Custom Movie App";
-    const customSubtitle = "Busca tus películas aquí";
-    render(<Header title={customTitle} subtitle={customSubtitle} />);
-
-    expect(screen.getByText(customTitle)).toBeInTheDocument();
-    expect(screen.getByText(customSubtitle)).toBeInTheDocument();
   });
 });
