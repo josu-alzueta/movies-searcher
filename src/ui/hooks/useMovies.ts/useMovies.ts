ee40fff3dc585e325513bad0c31a17acb58ce4c2
Josu Alzueta <josu.alzueta@biko2.com>
2025-06-24 18:39:19 +0200

    Refactor: adds useEffect to load moviesdata

--- src/ui/hooks/useMovies.ts/useMovies.ts
+++ src/ui/hooks/useMovies.ts/useMovies.ts
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Movie } from "@/types/movie";
 import moviesData from "@/data/movies.json";
 
 export const useMovies = () => {
   const [movies, setMovies] = useState<Movie[]>(moviesData);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      setMovies(moviesData);
+    };
+
+    loadMovies();
+  }, []);
 
   const onSearch = (search: string) => {
     setMovies(
